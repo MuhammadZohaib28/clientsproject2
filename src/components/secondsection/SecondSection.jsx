de822dd47c199dc48d67be416b79f5e2c0ddb12e
Zohaib <zohaibsoomro100@gmail.com>
2023-08-28 21:38:44 +0500

    changes made

--- src/components/secondsection/SecondSection.jsx
+++ src/components/secondsection/SecondSection.jsx
@@ -8,12 +8,34 @@ const SecondSection = () => {
   const [active2, setActive2] = useState(false);
   const [active3, setActive3] = useState(false);
   const [active4, setActive4] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0);
+
+  // const isActive = () => {
+  //   window.scrollY > 330 ? setActive(true) : setActive(false);
+  //   window.scrollY > 650 ? setActive2(true) : setActive2(false);
+  //   window.scrollY > 850 ? setActive3(true) : setActive3(false);
+  //   window.scrollY > 1000 ? setActive4(true) : setActive4(false);
+  // };
+
 
   const isActive = () => {
-    window.scrollY > 330 ? setActive(true) : setActive(false);
-    window.scrollY > 650 ? setActive2(true) : setActive2(false);
-    window.scrollY > 950 ? setActive3(true) : setActive3(false);
-    window.scrollY > 1150 ? setActive4(true) : setActive4(false);
+    const currentScrollY = window.scrollY;
+    
+    if (currentScrollY > prevScrollY) {
+      // Scrolling down
+      if (currentScrollY > 330) setActive(true);
+      if (currentScrollY > 650) setActive2(true);
+      if (currentScrollY > 850) setActive3(true);
+      if (currentScrollY > 1000) setActive4(true);
+    } else {
+      // Scrolling up
+      if (currentScrollY <= 1000) setActive4(false);
+      if (currentScrollY <= 850) setActive3(false);
+      if (currentScrollY <= 650) setActive2(false);
+      if (currentScrollY <= 330) setActive(false);
+    }
+  
+    setPrevScrollY(currentScrollY);
   };
 
 
